16f80d146fc10db4ce8a262f0aa5727f91b5cf9d
Tiago Patricio <69849680+tiagorpatricio@users.noreply.github.com>
2025-01-16 16:26:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,27 +1,50 @@
 //create web server
 const express = require('express');
 const app = express();
-
-//body-parser
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+const filePath = path.join(__dirname, 'comments.json');
+
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-//create in-memory database
-const comments = [];
-
-//get all comments
 app.get('/comments', (req, res) => {
-  res.json(comments);
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file.');
+      return;
+    }
+    res.send(data);
+  });
 });
 
-//create a comment
 app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  res.status(201).json(comment);
+  if (!req.body || !req.body.comment) {
+    res.status(400).send('Invalid request.');
+    return;
+  }
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file.');
+      return;
+    }
+
+    const comments = JSON.parse(data);
+    comments.push(req.body.comment);
+
+    fs.writeFile(filePath, JSON.stringify(comments), err => {
+      if (err) {
+        res.status(500).send('Error writing file.');
+        return;
+      }
+      res.send('Comment added.');
+    });
+  });
 });
 
-//start server
-app.listen(3000, () => {
-  console.log('Server started');
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
